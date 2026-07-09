2573717ae1a60fa31063e2ff412bb95c152992dc
abdalaziz <abdazizsaif15@gmail.com>
2026-07-09 12:28:49 +0300

    add get project path function

---
+++ src/controller/ProjectController.py
@@ -0,0 +1,20 @@
+from .BaseController import BaseController
+import os
+
+
+class ProjectController(BaseController):
+
+    def __init__(self):
+        super().__init__()
+
+    # get the project path from project id 
+    def get_project_path(self, project_id: str):
+        project_path = os.path.join(
+            self.files_dir,
+            project_id 
+        )
+
+        if not os.path.exists(project_path) : 
+            os.makedirs(project_path)
+
+        return project_path 
